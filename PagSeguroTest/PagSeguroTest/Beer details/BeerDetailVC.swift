bf28f9395ee949630ce7a54426ae447ca517a4fc
Camila Resende <aliamcami@gmail.com>
2019-03-01 19:19:31 -0300

    Background fixed image

--- PagSeguroTest/PagSeguroTest/Beer details/BeerDetailVC.swift
+++ PagSeguroTest/PagSeguroTest/Beer details/BeerDetailVC.swift
@@ -21,22 +21,35 @@ class BeerDetailVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setDetails()
+    }
+    
+    private func setDetails(){
         
         if let beer = self.beer{
             abvLabel.text = "Teor alcoólico: \(beer.abv)"
             ibuLabel.text = "Amargor: \(beer.ibu ?? 0)"
             nameLabel.text = beer.name
             taglineLabel.text = beer.tagline
-            descriptionTextView.text = beer.description
+            descriptionTextView.attributedText = formatLineHeight(beer.description)
             if image == nil{
                 headerImageView.setImage(from: beer.image_url)
             }else{
                 headerImageView.image = image
             }
+            
+//            headerImageView.image.insets
         }
     }
     
-    
+    private func formatLineHeight(_ text: String) -> NSAttributedString{
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = 7
+        let atributes = [NSAttributedString.Key.paragraphStyle: style,
+                         NSAttributedString.Key.font: UIFont.systemFont(ofSize: 15),
+                         NSAttributedString.Key.foregroundColor: UIColor.darkGray]
+        return NSAttributedString(string: text, attributes: atributes)
+    }
     
 
     /*
